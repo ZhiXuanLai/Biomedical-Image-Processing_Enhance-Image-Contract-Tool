80cf2e1ecb533a01ee57336df8fbfa6e538307d5
Tina @NTU <32099929+Tina-Tina@users.noreply.github.com>
2021-03-13 17:27:58 +0800

    construct a histogram for analysis

---
+++ BIP_HW_Q1_part1_Analysis.m
@@ -0,0 +1,52 @@
+clear; clf; clc;
+
+% Question 1 : Enhance Contrast
+% analyse the original image
+
+%mri-bright.256
+figure(1);
+
+ipfn = 'mri-bright.256';
+ipfp = fopen(ipfn, 'r');
+ipimg = fread(ipfp, [256,256], 'uchar');
+ipimg =ipimg';
+
+mri_bright=ipimg;
+subplot(2,2,1);
+image(mri_bright); %original
+colormap(gray(256));
+axis('image') ;
+title('mri-bright.256');
+
+subplot(2,2,3);
+h = zeros(256,1);
+for level=0:255
+    h( level+1)=sum(sum(mri_brigh=
+    =level));
+end
+
+bar(h); %histogram
+set(gca,'XLim',[0 255]);
+set(gca,'YLim',[0 3000]);
+
+%mri-dark.256
+ipfn = 'mri-dark.256';
+ipfp = fopen(ipfn, 'r');
+ipimg = fread(ipfp, [256,256], 'uchar');
+
+mri_dark=ipimg';
+subplot(2,2,2);
+image(mri_dark); %original
+colormap(gray(256));
+axis('image') ;
+title('mri-dark.256');
+
+subplot(2,2,4);
+k = zeros(256,1);
+for level=0:255
+    k( level+1)=sum(sum(ipimg==level));
+end
+bar(k); %histogram
+set(gca,'XLim',[0 255]);
+set(gca,'YLim',[0 3000]);
+fclose(ipfp);
